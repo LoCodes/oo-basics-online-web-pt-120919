cc751ceaf6c3cf5795df5fb5c5ea727ab19cbe6e
Loren Cipriano <loren25cipriano@gmail.com>
2020-01-27 06:36:43 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,20 +1,2 @@
 # Make your shoe class here!
 
-class Shoe
-  def initialize (brand)
-    @brand = brand
-  end
-  
-  def brand
-    @brand 
-  end
-  
-  def color= (color)
-    @color = color
-  end
-  
-  def color
-    @color
-  end
-  
-end
