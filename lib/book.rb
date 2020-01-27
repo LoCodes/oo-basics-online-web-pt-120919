86ee2273ac9e47e827da26a1c9ed90a836ef4c75
Loren Cipriano <loren25cipriano@gmail.com>
2020-01-27 06:32:19 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -37,4 +37,16 @@
   
 #end
 
-
+class Book 
+  attr_accessor :author, :page_count, :genre 
+  
+  def initialize(title)
+    @title = title
+  end
+  
+  def title 
+    @title
+  end
+  
+  def turn_page
+    puts 
